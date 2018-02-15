4cae14615185cc4d52c02835411bc640f2d88f6a
Gregor Harlan <mail@gh01.de>
2018-02-15 19:57:59 +0100

    glob

--- deploy.php
+++ deploy.php
@@ -6,12 +6,6 @@ require 'recipe/common.php';
 
 require __DIR__.'/deployer/config.php';
 
-require __DIR__.'/deployer/tasks/build/assets.php';
-require __DIR__.'/deployer/tasks/build/info.php';
-
-require __DIR__.'/deployer/tasks/database/migration.php';
-
-require __DIR__.'/deployer/tasks/build.php';
-require __DIR__.'/deployer/tasks/deploy.php';
-require __DIR__.'/deployer/tasks/release.php';
-require __DIR__.'/deployer/tasks/upload.php';
+foreach (glob(__DIR__.'/deployer/tasks/**/*.php', GLOB_NOSORT) as $path) {
+    require $path;
+}
