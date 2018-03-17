3aeb80382e60ab519052929ca278d881542f115c
Gregor Harlan <mail@gh01.de>
2018-03-17 13:57:23 +0100

    Deployment info in Redaxo verfügbar machen (#22)

---
+++ deployer/tasks/deploy/dump_info.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Deployer;
+
+desc('Dump info about current deployment');
+task('deploy:dump_info', function () {
+    $infos = [
+        'host' => get('hostname'),
+        'stage' => get('stage', false) ?: null,
+        'timestamp' => time(),
+        'branch' => runLocally('{{bin/git}} -C .build rev-parse --abbrev-ref HEAD'),
+        'commit' => runLocally('{{bin/git}} -C .build rev-parse HEAD'),
+    ];
+
+    $infos = json_encode($infos, JSON_PRETTY_PRINT);
+
+    run('mkdir -p {{release_path}}/{{data_dir}}/addons/ydeploy');
+    run('echo '.escapeshellarg($infos).' > {{release_path}}/{{data_dir}}/addons/ydeploy/info.json');
+});
