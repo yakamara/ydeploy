eaf7481fa4c0ac83b723c94113788121f7c7d89a
Gregor Harlan <mail@gh01.de>
2020-02-20 16:47:27 +0100

    Verbose-Option setzen für rex-console-Aufrufe

--- deployer/tasks/database/migration.php
+++ deployer/tasks/database/migration.php
@@ -6,7 +6,7 @@ desc('Migrate the database');
 task('database:migration', static function () {
     cd('{{release_path}}');
 
-    run('{{bin/php}} {{bin/console}} ydeploy:migrate');
+    run('{{bin/php}} {{bin/console}} ydeploy:migrate -v');
 
-    run('if [[ $({{bin/php}} {{bin/console}} list --raw | grep developer:sync) ]]; then {{bin/php}} {{bin/console}} developer:sync --force-files; fi');
+    run('if [[ $({{bin/php}} {{bin/console}} list --raw | grep developer:sync) ]]; then {{bin/php}} {{bin/console}} developer:sync --force-files -v; fi');
 });
