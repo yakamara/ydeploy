b6a126a1a9b77c142f18f53f6ca804fd7bcf97d0
Gregor Harlan <mail@gh01.de>
2022-03-12 13:18:43 +0100

    YForm: Geschützte Pages optimiert (#53)

--- boot.php
+++ boot.php
@@ -12,5 +12,12 @@ rex_extension::register('PAGE_BODY_ATTR', 'rex_ydeploy_handler::addBodyClasses')
 rex_extension::register('OUTPUT_FILTER', 'rex_ydeploy_handler::addBadge');
 
 if (rex_ydeploy::factory()->isDeployed()) {
+    $developer = rex_addon::get('developer');
+    if ($developer->isAvailable() && $developer->getConfig('yform_email')) {
+        $config = $this->getProperty('config');
+        $config['protected_pages']['yform']['email'] = null;
+        $this->setProperty('config', $config);
+    }
+
     rex_extension::register('PAGES_PREPARED', 'rex_ydeploy_handler::protectPages');
 }
