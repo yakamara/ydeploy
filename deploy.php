4e9209d5a6681f60911c3226c5cccfba1ea52083
gharlan <gharlan@users.noreply.github.com>
2023-03-19 11:16:52 +0000

    Apply php-cs-fixer changes

--- deploy.php
+++ deploy.php
@@ -2,9 +2,11 @@
 
 namespace Deployer;
 
+use RuntimeException;
+
 $version = ltrim(Deployer::get()->getConsole()->getVersion(), 'v');
 if (6 !== (int) $version) {
-    throw new \RuntimeException('YDeploy 1.x requires Deployer 6.x, but Deployer '.$version.' is used');
+    throw new RuntimeException('YDeploy 1.x requires Deployer 6.x, but Deployer '.$version.' is used');
 }
 
 /** @psalm-suppress MissingFile */
