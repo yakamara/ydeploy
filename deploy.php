b3521d196e3733c0d3897cec426dcbae07652c41
Gregor Harlan <mail@gh01.de>
2023-09-29 18:09:46 +0200

    psalm entfernt

--- deploy.php
+++ deploy.php
@@ -9,7 +9,6 @@ if (6 !== (int) $version) {
     throw new RuntimeException('YDeploy 1.x requires Deployer 6.x, but Deployer ' . $version . ' is used');
 }
 
-/** @psalm-suppress MissingFile */
 require 'recipe/common.php';
 
 require __DIR__ . '/deployer/config.php';
