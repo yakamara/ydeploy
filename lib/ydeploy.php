16cec2855dd02bff685dca605a1e72c4db89326c
Norbert Micheel <nm@lautundschoen.de>
2024-05-20 16:04:11 +0200

    rex_ydeploy class: stage can be null (#81)
    Co-authored-by: Gregor Harlan <mail@gh01.de>

--- lib/ydeploy.php
+++ lib/ydeploy.php
@@ -26,7 +26,7 @@ final class rex_ydeploy
         $info = rex_file::getCache($path);
 
         $this->host = rex_type::string($info['host']);
-        $this->stage = rex_type::string($info['stage']);
+        $this->stage = rex_type::nullOrString($info['stage']);
         $this->branch = rex_type::string($info['branch']);
         $this->commit = rex_type::string($info['commit']);
         $this->timestamp = rex_type::instanceOf(DateTimeImmutable::createFromFormat('U', $info['timestamp']), DateTimeImmutable::class)
