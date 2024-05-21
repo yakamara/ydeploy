a9dd9dd1a559f76b818af125d47fe0301d9bc49a
gharlan <gharlan@users.noreply.github.com>
2024-05-21 12:04:21 +0000

    Apply php-cs-fixer changes

--- deployer/tasks/build/info.php
+++ deployer/tasks/build/info.php
@@ -3,6 +3,6 @@
 namespace Deployer;
 
 desc('Displays info about build');
-task('build:info', function () {
+task('build:info', static function () {
     info('building <fg=magenta;options=bold>{{target}}</>');
 });
