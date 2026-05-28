bf091d337a4505442bce2945c37ecbfa7fdf4649
Alexander Walther <aw@alexplus.de>
2026-05-28 10:21:52 +0200

    Entferne PHPStan Ignorierregel für die Host-Klasse in der Konfigurationsdatei

--- deployer/config.php
+++ deployer/config.php
@@ -14,7 +14,6 @@ $rootPath = dirname(DEPLOYER_DEPLOY_FILE); /** @phpstan-ignore-line */
 $command = (new ArgvInput())->getFirstArgument();
 $localBuildDir = 'setup' !== $command && getenv('CI') === false;
 
-/** @phpstan-ignore-next-line */
 Deployer::get()->hosts = new class($rootPath, $localBuildDir) extends Host\HostCollection {
     public function __construct(
         private readonly string $rootPath,
